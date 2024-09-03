f1e2e491b69397a90aef0af9c111d35c9d310c75
Sanjay <146640686+san-jay-14@users.noreply.github.com>
2024-09-03 20:56:55 +0530

    font change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FirstHero from "./sections/FirstHero";
 import Navbar from "./sections/Navbar";
+import SecondHero from "./sections/SecondHero";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <FirstHero />
+      <SecondHero />
     </div>
   );
 }
---
+++ app/sections/SecondHero.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function FirstHero() {
+  return (
+    <div className="bg-black text-white flex justify-center items-center text-5xl leading-5">
+      <span className="m-6">
+        Building top quality software & apps for over fifteen years.
+      </span>
+    </div>
+  );
+}
