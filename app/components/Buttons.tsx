342faca2b8d3ada2b9502b758b0211992948fe51
Sanjay <146640686+san-jay-14@users.noreply.github.com>
2024-09-03 22:46:28 +0530

    btn change

--- app/components/Buttons.tsx
+++ app/components/Buttons.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
-
-export function PurpleActionButton({text}: {text: string}) {
-    return (
-        <button
-            className={`px-4 py-2 bg-[#df540c] text-white rounded-3xl hover:bg-purple-700 transition-colors}`}
-        >
-            {text}
-        </button>
-    );
+export function PurpleActionButton({ text }: { text: string }) {
+  return (
+    <button
+      className={`px-4 py-2 bg-[#df540c] text-white rounded-3xl hover:bg-purple-700 transition-colors}`}
+    >
+      {text}
+    </button>
+  );
 }
 
 export function TransparentBlackButton({ text }: { text: string }) {
-    return (
-        <button
-            className="px-4 py-2 bg-black text-white border border-white rounded-3xl hover:bg-gray-800 transition-colors"
-        >
-            {text}
-        </button>
-    );
+  return (
+    <button className="px-5 py-2 text-lg  text-white border border-white rounded-3xl hover:bg-gray-200 hover:text-black transition-colors bg-[#171717]">
+      {text}
+    </button>
+  );
 }
--- app/sections/SecondHero.tsx
+++ app/sections/SecondHero.tsx
@@ -3,19 +3,15 @@
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 import InfiniteCarousel from "./InfiniteCarousel";
+import { TransparentBlackButton } from "../components/Buttons";
 
 export default function FirstHero() {
   return (
     <div>
       <InfiniteCarousel />
-      <div
-        className="text-white flex justify-center items-center px-[2rem] py-[2.5]"
-        style={{
-          backgroundColor: "#171717 ",
-        }}
-      >
-        <div className="p-[4rem] mb-[4rem]">
-          <div className="p-[3rem]">
+      <div className="bg-[#171717] text-white flex justify-center items-center px-[2rem] py-[2.5]">
+        <div className="p-[4rem] mb-[4rem] ">
+          <div className="p-[3rem] ">
             <div className="max-w-[56rem] w-[100%] text-center">
               <p className="mt-[1rem] text-5xl font-semibold leading-[1.2]">
                 Building
@@ -26,11 +22,26 @@ export default function FirstHero() {
                 software & apps for over fifteen years.
               </p>
             </div>
-            <div className="text-center">buttons</div>
+            <div className="justify-center my-[4rem] flex flex-col gap-[2rem]">
+              <div className="flex gap-4 self-center">
+                {" "}
+                <TransparentBlackButton text="Web Applications" />
+                <TransparentBlackButton text="Mobile Applications" />
+                <TransparentBlackButton text="Business Platforms" />
+              </div>
+              <div className="flex gap-4 self-center">
+                <TransparentBlackButton text="UI/UX Design" />
+                <TransparentBlackButton text="Software Support" />
+              </div>
+            </div>
           </div>
-          <div className="text-center">
-            <div className="text-sm">text</div>
-            <div>button</div>
+          <div className="flex flex-col justify-center items-center ">
+            <div className="text-sm text-center max-w-[40rem] w-[100%] ">
+              Revolutionize your business with our mobile app and software
+              development expertise, delivering innovative solutions to elevate
+              your brand and boost your bottom line.
+            </div>
+            <div></div>
           </div>
         </div>
       </div>
